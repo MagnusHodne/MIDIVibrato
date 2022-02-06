ecbdfabcfaf45a7767af19517d59033c6597534a
Magnus H <me@magnushodne.com>
2022-02-06 18:55:38 +0100

    Made rate test more robust

--- Tests/FifoTest.cpp
+++ Tests/FifoTest.cpp
@@ -23,29 +23,45 @@ TEST_CASE("TestRMS", "[processors]")
 
 TEST_CASE("TestRate", "[processors]"){
     Utility::VibratoBuffer buffer(1);
-    juce::MidiBuffer midiBuffer;
+    juce::MidiBuffer noData, oneCrossing, twoCrossing, threeCrossing;
 
     REQUIRE(buffer.getRate() == 0);
-    midiBuffer.addEvent(juce::MidiMessage::controllerEvent(1, 1, 0), 1);
-    midiBuffer.addEvent(juce::MidiMessage::controllerEvent(1, 1, 127), 2);
-
-    buffer.calculateValues(midiBuffer);
-    REQUIRE(buffer.getRate() == 1);
-
-    midiBuffer.addEvent(juce::MidiMessage::controllerEvent(1, 1, 0), 3);
-    buffer.calculateValues(midiBuffer);
-    REQUIRE(buffer.getRate() == 2);
-
-    buffer.reset(2);
-
+    buffer.calculateValues(noData);
     REQUIRE(buffer.getRate() == 0);
 
-    buffer.calculateValues(midiBuffer);
-    REQUIRE(buffer.getRate() == 2.f/2);
+    oneCrossing.addEvent(juce::MidiMessage::controllerEvent(1, 1, 0), 1);
+    oneCrossing.addEvent(juce::MidiMessage::controllerEvent(1, 1, 127), 2);
+    threeCrossing = twoCrossing = oneCrossing;
+    twoCrossing.addEvent(juce::MidiMessage::controllerEvent(1, 1, 0), 3);
+    threeCrossing.addEvent(juce::MidiMessage::controllerEvent(1, 1, 0), 3);
+    threeCrossing.addEvent(juce::MidiMessage::controllerEvent(1,1,127), 4);
 
-    midiBuffer.addEvent(juce::MidiMessage::controllerEvent(1, 1, 127), 4);
-    buffer.calculateValues(midiBuffer);
-    REQUIRE(buffer.getRate() == (2.f + 3.f)/2.f);
+    REQUIRE(oneCrossing.getNumEvents() == 2);
+    REQUIRE(twoCrossing.getNumEvents() == 3);
+    REQUIRE(threeCrossing.getNumEvents() == 4);
+    //============================================
+    buffer.calculateValues(oneCrossing);
+    REQUIRE(buffer.getRate() == 1.f);
+    buffer.calculateValues(twoCrossing);
+    REQUIRE(buffer.getRate() == 2.f);
+    buffer.calculateValues(threeCrossing);
+    REQUIRE(buffer.getRate() == 3.f);
 
+    //============================================
+    buffer.reset(4);
+    buffer.calculateValues(oneCrossing);
+    buffer.calculateValues(noData);
+    buffer.calculateValues(oneCrossing);
+    buffer.calculateValues(noData);
 
+    REQUIRE(buffer.getRate() == 0.5f);
+
+    //============================================
+    buffer.reset(4);
+    buffer.calculateValues(oneCrossing);
+    buffer.calculateValues(oneCrossing);
+    buffer.calculateValues(oneCrossing);
+    buffer.calculateValues(noData);
+
+    REQUIRE(buffer.getRate() == 0.75f);
 }
