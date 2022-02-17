623da6f5e09303b752aa00c33dc6284ecc6d200c
Magnus H <me@magnushodne.com>
2022-02-17 22:50:53 +0100

    Made test compile

--- Tests/VibratoDetectorTest.cpp
+++ Tests/VibratoDetectorTest.cpp
@@ -6,23 +6,22 @@
 
 TEST_CASE("Test target rate", "[processors]")
 {
-    int samplesPerBlock = GENERATE(256, 512, 1024);
+    int blockSize = GENERATE(256, 512, 1024);
     double sampleRate = GENERATE(44100., 48000.);
     auto frequency = GENERATE(3.f, 4.f, 5.f, 6.f);
 
     auto inputController = 2;
     auto initialBufferSize = 190;
     VibratoDetector detector = VibratoDetector(initialBufferSize);
-    detector.setMetadata(sampleRate, samplesPerBlock);
-    detector.resetValues(initialBufferSize, 1.f);
+    detector.resetValues(sampleRate, blockSize);
     detector.setInputController(inputController);
 
-    Utility::MidiSineGenerator gen(inputController, frequency, sampleRate, samplesPerBlock);
+    Utility::MidiSineGenerator gen(inputController, frequency, sampleRate, blockSize);
     juce::MidiBuffer buffer;
     for(int i = 0; i < initialBufferSize; i++){
         buffer.clear();
         gen.fillBuffer(buffer);
-        detector.processMidi(buffer, samplesPerBlock);
+        detector.processMidi(buffer, blockSize);
     }
 
     REQUIRE(detector.getTargetRate() == Catch::Approx(frequency).margin(0.35));
@@ -40,7 +39,6 @@ TEST_CASE("Test rate slider value", "[processors]"){
 
     detector.setMinMaxRate(minRate, maxRate);
     detector.setInputController(inputController);
-    detector.setMetadata(sampleRate, samplesPerBlock);
     detector.resetValues(initialBufferSize, 1.0);
 
     Utility::MidiSineGenerator gen(inputController, frequency, sampleRate, samplesPerBlock);
@@ -52,7 +50,7 @@ TEST_CASE("Test rate slider value", "[processors]"){
     }
 
     auto frequencyAsMidiValue = juce::jmap(std::clamp(frequency, minRate, maxRate), minRate, maxRate, 0.f, 127.f);
-    auto midiRateValue = detector.getRate();
+    auto midiRateValue = detector.getFrequency();
 
     REQUIRE(midiRateValue == Catch::Approx(frequencyAsMidiValue).margin(2));
 }
@@ -60,18 +58,18 @@ TEST_CASE("Test rate slider value", "[processors]"){
 TEST_CASE("Test max amplitude", "[processors]")
 {
     VibratoDetector detector = VibratoDetector(4);
-    int samplesPerBlock = 256;
+    int blockSize = 256;
     double sampleRate = 48000;
-    detector.setMetadata(sampleRate, samplesPerBlock);
     auto inputController = 2;
+    detector.resetValues(sampleRate, blockSize);
     detector.setInputController(inputController);
 
     juce::MidiBuffer buffer;
     for(int i = 0; i < 4; i++){
         buffer.addEvent(juce::MidiMessage::controllerEvent(1, inputController, 0), 1);
         buffer.addEvent(juce::MidiMessage::controllerEvent(1, inputController, 127), 128);
-        detector.processMidi(buffer, samplesPerBlock);
+        detector.processMidi(buffer, blockSize);
     }
 
-    REQUIRE(detector.getAmplitude() == 127);
+    REQUIRE(detector.getRms() == 127);
 }
